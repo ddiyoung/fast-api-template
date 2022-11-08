41e4629a552f15d2b6a89477b0a96275ebbdec44
ddiyoung <wsw3998@naver.com>
2022-11-09 02:16:38 +0900

    [* UserSchema.u_id int형 변환]

--- app/schemas/pydantic/User.py
+++ app/schemas/pydantic/User.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel
 
 
 class UserPostRequestSchema(BaseModel):
-    user_id: str
+    user_id: int
 
 
 class UserSchema(UserPostRequestSchema):
     user_name: str
     user_department: str
-    user_auth: str
+    user_auth: int
